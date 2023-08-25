3c3fd6bda443d2a4cbfc95cb52738d50d06d2814
farheenfathimaa <farheennfathima@gmail.com>
2023-08-25 16:37:04 +0530

    Write a program to produce the following output:    1   2 3  4 5 6 7 8 9 0

--- number_pattern.c
+++ number_pattern.c
@@ -2,17 +2,18 @@
    1
   2 3
  4 5 6
-7 8 9 10*/
+7 8 9 0*/
 #include<stdio.h>
 int main(){
     int currentNum=1,i,j,numRows=4;
     for(i=1;i<=numRows;i++){
         for(j=1;j<=numRows-i;j++){
-            printf(" ");
+            printf("  ");
         }
         for(j=1;j<=i;j++){
-            printf("%d ",currentNum);
+            printf("%2d  ",currentNum);
             currentNum++;
+            if(currentNum==10) currentNum=0;
         }
         printf("\n");
     }
